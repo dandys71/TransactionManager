09b81653867383c1e0400ab23294f73c65856fa0
VojtaVeve18 <vojta.veverka3@gmail.com>
2026-04-09 14:43:39 +0200

    zkouška

--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -3,3 +3,10 @@ import {Router} from 'express';
 import { createEventTemplate } from "../controllers/eventsController.js";
 
 export const router = Router();
+
+//[POST]triggerRandomEvent
+router.post("/simulation/shift", eventsController.shiftSimulation);
+
+router.post("/previewNextWindow", eventsController.)fdfs
+
+export default router;
