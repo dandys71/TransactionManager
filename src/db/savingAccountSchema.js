ec32d73ab794d532863ca73b7658c4e0c24b5f09
Ondra Flégl <c23flegl@oakostelec.cz>
2026-09-02 14:53:01 +0200

    Flégl - Savingaccount schema do DB

---
+++ src/db/savingAccountSchema.js
@@ -0,0 +1,15 @@
+import { pgTable, text, integer, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const savingAccounts = pgTable("savingAccounts", {
+    accountId: text("account_id").primaryKey(),
+    userId: text("user_id").notNull(),
+    institutionId: text("institution_id"),
+    accountNumber: text("account_number").notNull(),
+    name: text("name").notNull(),
+    currency: text("currency").notNull(),
+    balance: integer("balance").notNull(),
+    isActive: boolean("is_active").notNull().default(true),
+    createdAt: timestamp("created_at").notNull(),
+    type: text("type")
+
+});
